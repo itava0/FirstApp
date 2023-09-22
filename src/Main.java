1098dd4d864e94186d80703fb99ff39116d6e26b
Ivan Tavarez <ivantavarez2@gmail.com>
2023-09-22 11:09:23 -0400

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double value1 = 100.00;
+        double value2 = 50.00;
+        double result = 0.00;
+        char opCode = 'd';
+
+        if(opCode == 'a') {
+            result = value1 + value2;
+        } else if(opCode == 's') {
+            result = value1 - value2;
+        } else if(opCode == 'm') {
+            result = value1 * value2;
+        } else if (opCode == 'd') {
+            result = value1 / value2;
+        } else {
+            System.out.println("Invalid opCode " + opCode);
+            result = 0.00;
+        }
+
+        System.out.println(result);
     }
 }
